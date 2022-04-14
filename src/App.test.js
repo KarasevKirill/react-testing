c1111f138c6f4442d785a6cf783fa7c8f82aba69
Kirill Karasev <karasev.kirill.a@gmail.com>
2022-04-15 00:33:41 +1000

    fireEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,64 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe("App", () => {
-    it("Render App component", () => {
+    // it("Render App component", () => {
+    //     render(<App />);
+    //     screen.debug();
+
+    //     expect(screen.getByText(/Search:/i)).toBeInTheDocument();
+    //     expect(screen.getByRole("textbox")).toBeInTheDocument();
+    //     expect(screen.getByLabelText(/Search:/i)).toBeInTheDocument();
+    //     expect(screen.getByPlaceholderText("search...")).toBeInTheDocument();
+    //     expect(screen.getByAltText("search image")).toBeInTheDocument(); // search image by alt tag
+    //     expect(screen.getByDisplayValue("")).toBeInTheDocument(); // search value attribute. Checking form's default values
+
+    //     expect(screen.queryByText(/Search for react/i)).toBeNull();
+    // });
+
+    // it("Async", async () => {
+    //     render(<App />);
+
+    //     expect(screen.queryByText(/Logged in as/i)).toBeNull();
+    //     expect(await screen.findByText(/Logged in as/i)).toBeInTheDocument();
+
+    //     expect(screen.getByAltText("search image")).toHaveClass("my-class");
+
+    //     expect(screen.getByLabelText(/search/i)).toBeRequired();
+    //     //expect(screen.getByLabelText(/search/i)).not.toBeRequired();
+
+    //     expect(screen.getByLabelText(/search/i)).toBeEmptyDOMElement();
+    //     expect(screen.getByLabelText(/search/i)).toHaveAttribute("id");
+    // });   
+});
+
+describe("Events", () => {
+    test("change input value", async () => {
         render(<App />);
-        screen.debug();
 
-        expect(screen.getByText(/Search:/i)).toBeInTheDocument();
-        expect(screen.getByRole("textbox")).toBeInTheDocument();
-        expect(screen.getByLabelText(/Search:/i)).toBeInTheDocument();
-        expect(screen.getByPlaceholderText("search...")).toBeInTheDocument();
-        expect(screen.getByAltText("search image")).toBeInTheDocument(); // search image by alt tag
-        expect(screen.getByDisplayValue("")).toBeInTheDocument(); // search value attribute. Checking form's default values
+        // waiting for asynchronous operations in the component
+        await screen.findByText(/logged in as/i);
 
-        expect(screen.queryByText(/Search for react/i)).toBeNull();
+        expect(screen.queryByText(/React/)).toBeNull();
+
+        fireEvent.change(screen.getByRole("textbox"), {
+            target: {value: "React"}
+        });
+
+        expect(screen.queryByText(/React/)).toBeInTheDocument();
     });
 
-    it("Async", async () => {
-        render(<App />);
+    it("input focus", () => {
+        const { getByTestId } = render(
+            <input type="text" data-testid="simple-input" />
+        );
 
-        expect(screen.queryByText(/Logged in as/i)).toBeNull();
-        expect(await screen.findByText(/Logged in as/i)).toBeInTheDocument();
+        const input = getByTestId("simple-input");
 
-        expect(screen.getByAltText("search image")).toHaveClass("my-class");
+        expect(input).not.toHaveFocus();
 
-        expect(screen.getByLabelText(/search/i)).toBeRequired();
-        //expect(screen.getByLabelText(/search/i)).not.toBeRequired();
-        
-        expect(screen.getByLabelText(/search/i)).toBeEmpty();
-        expect(screen.getByLabelText(/search/i)).toHaveAttribute("id");
+        input.focus();
+
+        expect(input).toHaveFocus();
     });
 });
