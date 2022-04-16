e09967654d82267dfd05928824f76a70e0dfa60e
Kirill Karasev <karasev.kirill.a@gmail.com>
2022-04-16 17:36:37 +1000

    Testing async functions with get data

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import App from './App';
 
 describe("App", () => {
@@ -35,112 +36,165 @@ describe("App", () => {
 
 describe("Events", () => {
 
-    // fireEvent
-    test("change input value", async () => {
-        render(<App />);
+    // // fireEvent
+    // test("change input value", async () => {
+    //     render(<App />);
 
-        // waiting for asynchronous operations in the component
-        await screen.findByText(/logged in as/i);
+    //     // waiting for asynchronous operations in the component
+    //     await screen.findByText(/logged in as/i);
 
-        expect(screen.queryByText(/React/)).toBeNull();
+    //     expect(screen.queryByText(/React/)).toBeNull();
 
-        fireEvent.change(screen.getByRole("textbox"), {
-            target: {value: "React"}
-        });
+    //     fireEvent.change(screen.getByRole("textbox"), {
+    //         target: {value: "React"}
+    //     });
 
-        expect(screen.queryByText(/React/)).toBeInTheDocument();
+    //     expect(screen.queryByText(/React/)).toBeInTheDocument();
+    // });
+
+    // it("input focus", () => {
+    //     const { getByTestId } = render(
+    //         <input type="text" data-testid="simple-input" />
+    //     );
+
+    //     const input = getByTestId("simple-input");
+
+    //     expect(input).not.toHaveFocus();
+
+    //     input.focus();
+
+    //     expect(input).toHaveFocus();
+    // });
+    // // fireEvent
+
+    // // userEvent (priority over fireSelect)
+    // test("change input value", async () => {
+    //     render(<App />);
+
+    //     // waiting for asynchronous operations in the component
+    //     await screen.findByText(/logged in as/i);
+
+    //     expect(screen.queryByText(/React/)).toBeNull();
+
+    //     userEvent.type(screen.getByRole("textbox"), "React");
+
+    //     expect(screen.queryByText(/React/)).toBeInTheDocument();
+    // });
+
+    // it("checkbox click", () => {
+    //     const { container } = render(<input type="checkbox" />);
+    //     const checkbox = container.firstChild;
+
+    //     expect(checkbox).not.toBeChecked();
+
+    //     userEvent.click(checkbox);
+
+    //     expect(checkbox).toBeChecked();
+    // });
+
+    // it("checkbox double click", () => {
+    //     const onChange = jest.fn();
+    //     const { container } = render(<input type="checkbox" onChange={onChange} />);
+    //     const checkbox = container.firstChild;
+
+    //     expect(checkbox).not.toBeChecked();
+
+    //     userEvent.dblClick(checkbox);
+
+    //     expect(onChange).toHaveBeenCalledTimes(2);
+
+    //     expect(checkbox).not.toBeChecked();
+    // });
+
+    // it("focus with button Tab", () => {
+    //     const { getAllByTestId } = render(
+    //         <div>
+    //             <input data-testid="element" type="checkbox" />
+    //             <input data-testid="element" type="radio" />
+    //             <input data-testid="element" type="text" />
+    //         </div>
+    //     );
+
+    //     const [checkbox, radio, text] = getAllByTestId("element");
+
+    //     userEvent.tab();
+    //     expect(checkbox).toHaveFocus();
+
+    //     userEvent.tab();
+    //     expect(radio).toHaveFocus();
+
+    //     userEvent.tab();
+    //     expect(text).toHaveFocus();
+    // });
+
+    // it("select option", () => {
+    //     const { selectOptions, getByRole, getByText } = render(
+    //         <select>
+    //             <option value="1">One</option>
+    //             <option value="2">Two</option>
+    //             <option value="3">Three</option>
+    //         </select>
+    //     );
+
+    //     userEvent.selectOptions(getByRole("combobox"), "1");
+    //     expect(getByText("One").selected).toBeTruthy(); // First element is selected
+
+    //     userEvent.selectOptions(getByRole("combobox"), "2");
+    //     expect(getByText("Two").selected).toBeTruthy();
+    //     expect(getByText("One").selected).toBeFalsy(); // First element is no longer selected
+    // });
+    // // userEvent
+});
+
+jest.mock("axios");
+
+const hits = [
+    { objectID: "1", title: "Angular" },
+    { objectID: "2", title: "React" },
+    { objectID: "3", title: "Vue" }
+];
+
+describe("App", () => {
+    it("fetches data from API", async () => {
+
+        axios.get.mockImplementationOnce(() => Promise.resolve({ data: { hits } }));
+
+        const { getByRole, findAllByRole } = render(<App />);
+
+        userEvent.click(getByRole("button"));
+
+        const items = await findAllByRole("listitem");
+
+        expect(items).toHaveLength(3);
+        expect(axios.get).toHaveBeenCalledTimes(1); // how many times was called
+        expect(axios.get).toHaveBeenCalledWith("http://hn.algolia.com/api/v1/search?query=React"); // with what parameters it was called
     });
 
-    it("input focus", () => {
-        const { getByTestId } = render(
-            <input type="text" data-testid="simple-input" />
-        );
+    it("fetches data from API with error", async () => {
 
-        const input = getByTestId("simple-input");
+        axios.get.mockImplementationOnce(() => Promise.reject(new Error()));
 
-        expect(input).not.toHaveFocus();
+        const { getByRole, findByText } = render(<App />);
 
-        input.focus();
+        userEvent.click(getByRole("button"));
 
-        expect(input).toHaveFocus();
-    });
-    // fireEvent
+        const message = await findByText(/Something went wrong.../);
 
-    // userEvent (priority over fireSelect)
-    test("change input value", async () => {
-        render(<App />);
-
-        // waiting for asynchronous operations in the component
-        await screen.findByText(/logged in as/i);
-
-        expect(screen.queryByText(/React/)).toBeNull();
-
-        userEvent.type(screen.getByRole("textbox"), "React");
-
-        expect(screen.queryByText(/React/)).toBeInTheDocument();
+        expect(message).toBeInTheDocument();
     });
 
-    it("checkbox click", () => {
-        const { container } = render(<input type="checkbox" />);
-        const checkbox = container.firstChild;
+    it("fetches data from API with function act", async () => {
 
-        expect(checkbox).not.toBeChecked();
+        const promise = Promise.resolve({ data: { hits } });
 
-        userEvent.click(checkbox);
+        axios.get.mockImplementationOnce(() => promise);
 
-        expect(checkbox).toBeChecked();
+        const { getByRole, getAllByRole } = render(<App />);
+
+        userEvent.click(getByRole("button"));
+
+        await act(() => promise);
+
+        expect(getAllByRole("listitem")).toHaveLength(3);
     });
-
-    it("checkbox double click", () => {
-        const onChange = jest.fn();
-        const { container } = render(<input type="checkbox" onChange={onChange} />);
-        const checkbox = container.firstChild;
-
-        expect(checkbox).not.toBeChecked();
-
-        userEvent.dblClick(checkbox);
-
-        expect(onChange).toHaveBeenCalledTimes(2);
-
-        expect(checkbox).not.toBeChecked();
-    });
-
-    it("focus with button Tab", () => {
-        const { getAllByTestId } = render(
-            <div>
-                <input data-testid="element" type="checkbox" />
-                <input data-testid="element" type="radio" />
-                <input data-testid="element" type="text" />
-            </div>
-        );
-
-        const [checkbox, radio, text] = getAllByTestId("element");
-
-        userEvent.tab();
-        expect(checkbox).toHaveFocus();
-
-        userEvent.tab();
-        expect(radio).toHaveFocus();
-
-        userEvent.tab();
-        expect(text).toHaveFocus();
-    });
-
-    it("select option", () => {
-        const { selectOptions, getByRole, getByText } = render(
-            <select>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </select>
-        );
-
-        userEvent.selectOptions(getByRole("combobox"), "1");
-        expect(getByText("One").selected).toBeTruthy(); // First element is selected
-
-        userEvent.selectOptions(getByRole("combobox"), "2");
-        expect(getByText("Two").selected).toBeTruthy();
-        expect(getByText("One").selected).toBeFalsy(); // First element is no longer selected
-    });
-    // userEvent
 });
